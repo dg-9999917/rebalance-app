b24150974de7143fdfe35ddc53f240c88a05b9e0
dg-9999917 <292691145+dg-9999917@users.noreply.github.com>
2026-07-07 00:08:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rebalance-v21';
+const CACHE_NAME = 'rebalance-v38';
 const SHELL = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
